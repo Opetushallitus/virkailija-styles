56bcf288251bd77a5e9856b69980b1891f81a34f
Esa Taskinen <esa.taskinen@visma.com>
2017-03-30 09:56:59 +0300

    Updated config

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,13 +1,8 @@
 module.exports = {
   use: [
     'postcss-smart-import',
-    'postcss-cssnext',
-    'cssnano'
+    'postcss-cssnext'
   ],
-  "cssnano": {
-    autoprefixer: false,
-    zindex: false
-  },
   input: 'src/index.css',
   output: 'oph-styles.css'
 };
